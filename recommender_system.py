591791b82919831de260339df2e4d23641631bc0
smithaeshwarahalliramesh <44306959+smithaeshwarahalliramesh@users.noreply.github.com>
2019-08-08 22:41:13 -0700

    Update recommender_system.py

--- recommender_system.py
+++ recommender_system.py
@@ -1,7 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import numpy as np
